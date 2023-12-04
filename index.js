89851c01e65acc2d76246cc53657bcd582cd5112
kavya896 <kavyareddyk1@gmail.com>
2023-12-04 12:09:14 +0530

    env changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var note = require("./router/note.js")
 var mongoose = require("mongoose")
 var cors = require("cors")
 require("dotenv").config()
-
+var PORT = process.env.PORT || 6010
 app.use(cors())
 app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({limit:'50mb',extended:true}))
@@ -21,6 +21,6 @@ mongoose.connect(process.env.DATA_BASE).then(
     }
 )
 
-app.listen(5000,(req,res)=>{
+app.listen(PORT,(req,res)=>{
     console.log("Listening at port 5000")
 })
